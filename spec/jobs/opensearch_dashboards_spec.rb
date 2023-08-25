11cb0b9ff6168a8f211935b4bc0697485fdf70c6
Mark Boyd <mark.boyd@gsa.gov>
2023-08-25 17:38:02 -0400

    add unit tests

--- spec/jobs/opensearch_dashboards_spec.rb
+++ spec/jobs/opensearch_dashboards_spec.rb
@@ -24,6 +24,139 @@ describe 'opensearch_dashboards job' do
         expect(config['opensearch.requestTimeout']).to eq(300000)
         expect(config['opensearch.shardTimeout']).to eq(30000)
       end
+
+      it 'does not configure optional settings' do
+        expect(config['opensearch.username']).to be_nil
+        expect(config['opensearch.password']).to be_nil
+        expect(config['opensearch.ssl.certificate']).to be_nil
+        expect(config['opensearch.ssl.key']).to be_nil
+        expect(config['opensearch.ssl.certificateAuthorities']).to be_nil
+      end
+    end
+
+    describe 'with port settings' do
+      context 'with no settings' do
+        let(:config) do
+          config = YAML.load(template.render({}))
+        end
+        
+        it 'sets port correctly' do
+          expect(config['opensearch.hosts']).to eq('https://localhost:9200')
+        end
+      end
+
+      context 'with manifest settings' do
+        let(:manifest) do
+          {
+            'opensearch_dashboards' => {
+              'opensearch' => {
+                'host' => 'dashboard',
+                'port' => '2222'
+              }
+            }
+          }
+        end
+
+        let(:config) do
+          config = YAML.load(template.render(manifest))
+        end
+        
+        it 'sets port correctly' do
+          expect(config['opensearch.hosts']).to eq('https://dashboard:2222')
+        end
+      end
+
+      context 'with node link' do
+        let(:manifest) do
+          {
+            'opensearch_dashboards' => {
+              'opensearch' => {
+                'ssl' => {
+                  'certificate' => 'fake-cert',
+                  'private_key' => 'fake-key'
+                }
+              }
+            }
+          }
+        end
+  
+        let(:link) do
+          Bosh::Template::Test::Link.new(
+            name: 'opensearch',
+            instances: [
+              Bosh::Template::Test::LinkInstance.new(),
+            ],
+            properties: {
+              'opensearch' => {
+                'port' => '1111',
+                'node' => {
+                  'ssl' => {
+                    'ca' => 'fake-ca'
+                  }
+                }
+              }
+            }
+          )
+        end
+  
+        let(:config) do
+          config = YAML.load(template.render(manifest, consumes: [link]))
+        end
+        
+        it 'sets port correctly' do
+          expect(config['opensearch.hosts']).to eq('https://localhost:1111')
+        end
+      end
+    end
+
+    describe 'with opensearch SSL settings' do
+      let(:manifest) do
+        {
+          'opensearch_dashboards' => {
+            'opensearch' => {
+              'ssl' => {
+                'certificate' => 'fake-cert',
+                'private_key' => 'fake-key'
+              }
+            }
+          }
+        }
+      end
+
+      let(:link) do
+        Bosh::Template::Test::Link.new(
+          name: 'opensearch',
+          instances: [
+            Bosh::Template::Test::LinkInstance.new(),
+          ],
+          properties: {
+            'opensearch' => {
+              'port' => '9200',
+              'node' => {
+                'ssl' => {
+                  'ca' => 'fake-ca'
+                }
+              }
+            }
+          }
+        )
+      end
+
+      let(:config) do
+        config = YAML.load(template.render(manifest, consumes: [link]))
+      end
+
+      it 'sets the SSL certificate for Opensearch communication' do
+        expect(config['opensearch.ssl.certificate']).to eq('/var/vcap/jobs/opensearch_dashboards/config/ssl/opensearch-dashboard.crt')
+      end
+
+      it 'sets the SSL private key for Opensearch communication' do
+        expect(config['opensearch.ssl.key']).to eq('/var/vcap/jobs/opensearch_dashboards/config/ssl/opensearch-dashboard.key')
+      end
+
+      it 'sets the SSL CA for Opensearch communication' do
+        expect(config['opensearch.ssl.certificateAuthorities']).to eq(['/var/vcap/jobs/opensearch_dashboards/config/ssl/opensearch.ca'])
+      end
     end
   end
 end
