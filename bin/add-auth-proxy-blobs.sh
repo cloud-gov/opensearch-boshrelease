72377f275eef0ab8529b8c525e49cd851ce8dff5
Mark Boyd <mark.boyd@gsa.gov>
2024-11-06 12:21:20 -0500

    update script for add python wheel blobs

--- bin/add-auth-proxy-blobs.sh
+++ bin/add-auth-proxy-blobs.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-AUTH_PROXY_SRC_DIR=$1
+WHEEL_SRC_DIR=$1
+BLOB_PREFIX=$2
 
-if [ -z "$AUTH_PROXY_SRC_DIR" ]; then
+if [ -z "$WHEEL_SRC_DIR" ]; then
   echo "path to opensearch-dashboards-cf-auth-proxy source code required as first argument. exiting"
   exit 1
 fi
 
-find "$AUTH_PROXY_SRC_DIR" \
+if [ -z "$BLOB_PREFIX" ]; then
+  echo "blob prefix required as second argument. exiting"
+  exit 1
+fi
+
+find "$WHEEL_SRC_DIR" \
   -type f \
   -name "*.whl" \
-  -exec bash -c 'bosh add-blob {} auth-proxy/$(basename {})' \;
+  -exec bash -c 'path="$1"; blob_prefix="$2"; file_name=$(basename $1); bosh add-blob $path "$blob_prefix/$file_name"' shell {} "$BLOB_PREFIX" \;
