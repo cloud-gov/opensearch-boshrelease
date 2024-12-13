e899dc1c06d016ed54195ecdcb8641256333c394
Mark Boyd <mark.boyd@gsa.gov>
2024-12-13 11:19:16 -0500

    fix quoting in script so that tenant description is updated correctly

--- jobs/upload_tenant/templates/bin/run.sh
+++ jobs/upload_tenant/templates/bin/run.sh
@@ -18,22 +18,23 @@ KEY="--key ${JOB_DIR}/config/ssl/opensearch-admin.key"
 cf api "<%= api %>"
 cf auth "<%= client %>" "<%= password %>" --client-credentials
 
-for org in `cf orgs| tail -n +4`; do
+for org in `cf orgs | tail -n +4`; do
 ORG_GUID=\"$(cf org ${org} --guid)\"
 org_quoted=\"$org\"
 curl -X PUT \
   ${CA} ${CERT} ${KEY} \
   -s https://localhost:9200/_plugins/_security/api/tenants/${org} \
-  -H 'Content-Type: application/json' -d'{ "description": "A tenant for the ${org} team." }'
+  -H 'Content-Type: application/json' -d "{ \"description\": \"A tenant for the ${org} team.\" }"
+  
 curl -X PUT \
   ${CA} ${CERT} ${KEY} \
   -s https://localhost:9200/_plugins/_security/api/roles/${org}-tenant \
-  -H 'Content-Type: application/json' -d'{"tenant_permissions":[{"tenant_patterns": ['"${org_quoted}"'],"allowed_actions": ["kibana_all_write"]}]}'
+  -H 'Content-Type: application/json' -d '{"tenant_permissions":[{"tenant_patterns": ['"${org_quoted}"'],"allowed_actions": ["kibana_all_write"]}]}'
 
 curl -X PUT \
   ${CA} ${CERT} ${KEY} \
   -s https://localhost:9200/_plugins/_security/api/rolesmapping/${org}-tenant \
-  -H 'Content-Type: application/json' -d'{"backend_roles": ['${ORG_GUID}']}'
+  -H 'Content-Type: application/json' -d '{"backend_roles": ['${ORG_GUID}']}'
 
 done
 
